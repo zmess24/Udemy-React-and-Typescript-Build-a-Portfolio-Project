eab8979ada3ceafe83881f06049193a753a93c01
Zachary Messinger <zdmessinger@gmail.com>
2022-10-11 19:43:20 -0700

    Updated

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <form action="">
-        <input type="text" />
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
