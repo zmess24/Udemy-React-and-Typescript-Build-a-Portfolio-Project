534b47c9ae24816a7d84a9f2c56c604554dbf863
Zachary Messinger <zacharymessinger@qm-zacharymessinger-m1.lan>
2022-11-13 18:58:07 -0800

    updated

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,27 +1,54 @@
-import * as esbuild from 'esbuild-wasm';
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
-  return {
-    name: 'unpkg-path-plugin',
-    setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
-      });
+	return {
+		name: "unpkg-path-plugin",
+		setup(build: esbuild.PluginBuild) {
+			build.onResolve({ filter: /.*/ }, async (args: any) => {
+				console.log("onResolve", args);
+				if (args.path === "index.js") {
+					return { path: args.path, namespace: "a" };
+				}
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+				if (args.path.includes("./") || args.path.includes("../")) {
+					return {
+						namespace: "a",
+						path: new URL(args.path, args.importer + "/").href,
+					};
+				}
 
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: `
-              import message from 'tiny-test-pkg';
+				return {
+					namespace: "a",
+					path: `https://unpkg.com/${args.path}`,
+				};
+				// else if (args.path === "tiny-test-pkg") {
+				// 	return {
+				// 		path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+				// 		namespace: "a",
+				// 	};
+				// }
+			});
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
+				console.log("onLoad", args);
+
+				if (args.path === "index.js") {
+					return {
+						loader: "jsx",
+						contents: `
+              const message = require('medium-test-pkg')
               console.log(message);
             `,
-          };
-        }
-      });
-    },
-  };
+					};
+				}
+
+				const { data } = await axios.get(args.path);
+				return {
+					loader: "jsx",
+					contents: data,
+				};
+			});
+		},
+	};
 };
