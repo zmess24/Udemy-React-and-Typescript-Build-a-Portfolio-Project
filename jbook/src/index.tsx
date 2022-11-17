f5b2b39bfaef656164b86258bafd7320182aa304
Zac Messinger <zdmessinger@gmail.com>
2022-11-16 18:19:30 -0800

    Finished Section 10

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -25,6 +25,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -39,14 +41,29 @@ const App = () => {
       }
     });
 
-    setCode(result.outputFiles[0].text);
-    
-    try {
-      eval(result.outputFiles[0].text)
-    } catch (err) {
-      console.log(err)
-    }
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+            window.addEventListener('message', (e) => {
+              try {
+                eval(e.data)
+              } catch (err) {
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red"><h4>Runtime Error:</h4>' + err + '</div>'
+                console.log(err)
+              }
+            }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -57,8 +74,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe sandbox="allow-same-origin" src="/test.html" />
+      <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
