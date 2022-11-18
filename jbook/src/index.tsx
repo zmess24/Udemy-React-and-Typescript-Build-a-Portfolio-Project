4aa6c9ffcc9e616abdef435a71d966c76d914676
Zac Messinger <zdmessinger@gmail.com>
2022-11-17 18:57:29 -0800

    finished module

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -67,6 +68,9 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor 
+      initialValue="const a = 1;"
+      onChange={(value) => setInput(value)}/>
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
