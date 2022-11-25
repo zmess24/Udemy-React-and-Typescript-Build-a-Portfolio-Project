a4a2a916aa5ec6ffe367225c5a38c3f4586de451
Zac Messinger <zdmessinger@gmail.com>
2022-11-25 12:57:14 -0500

    finished module

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,16 +1,17 @@
 // import { createRoot } from 'react-dom/client';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-// import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor/>
-      {/* <CodeCell /> */}
-      {/* <CodeCell /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor/>
+      </div>
+    </Provider>
   );
 };
 
