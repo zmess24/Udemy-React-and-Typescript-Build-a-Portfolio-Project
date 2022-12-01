4078fc374d6f91e2359d8b70deb6a6975a142a2e
Zac Messinger <zdmessinger@gmail.com>
2022-11-30 21:04:23 -0500

    finished modules

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,21 +1,21 @@
 // import { createRoot } from 'react-dom/client';
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './state';
-import TextEditor from './components/text-editor';
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./state";
+import CellList from "./components/cell-list";
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <div>
-        <TextEditor/>
-      </div>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<div>
+				<CellList />
+			</div>
+		</Provider>
+	);
 };
 
-const container = document.querySelector('#root');
+const container = document.querySelector("#root");
 // const root = createRoot(!container);
 // root.render(<App/>)
 
--- jbook/src/state/reducers/cellsReducers.ts
+++ jbook/src/state/reducers/cellsReducers.ts
@@ -68,7 +68,7 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
 		default:
 			return state;
 	}
-});
+}, initialState);
 
 // Random ID Generation Function
 const randomId = () => {
