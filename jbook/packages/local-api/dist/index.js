5c8911e6773667de4edb5d5c0ff4dbb116b9c922
Zac Messinger <zdmessinger@gmail.com>
2023-01-11 18:51:56 -0800

    Fixed error

--- jbook/packages/local-api/dist/index.js
+++ jbook/packages/local-api/dist/index.js
@@ -4,8 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
+    // Middleware
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: "http://127.0.0.1:3000",
+        ws: true,
+        logLevel: "silent",
+    }));
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on("error", reject);
     });
--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const serve = (port: number, filename: string, dir: string) => {
 	const app = express();
+
+	// Middleware
+	app.use(
+		createProxyMiddleware({
+			target: "http://127.0.0.1:3000",
+			ws: true,
+			logLevel: "silent",
+		})
+	);
 
 	return new Promise<void>((resolve, reject) => {
 		app.listen(port, resolve).on("error", reject);
